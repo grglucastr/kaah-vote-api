c14b3bf17f837935fd2c5fc528c3ad87ec9d31a6
George Nunes <georgebnunes@gmail.com>
2025-08-11 15:17:06 -0300

    feat: candidates handlers and routes

---
+++ cmd/api/candidates.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func (app *application) postSessionCandidatesHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) getSessionCandidatesHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) deleteSessionCandidateHandler(w http.ResponseWriter, r *http.Request) {
+
+}
